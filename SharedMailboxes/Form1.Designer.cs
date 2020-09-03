5c86a40dc1881d0ff567e14e412c10dcd5a83c77
Markus Gärtner <gaertner.brilon@gmail.com>
2020-09-03 16:09:11 +0200

    20200903 16:09

--- SharedMailboxes/Form1.Designer.cs
+++ SharedMailboxes/Form1.Designer.cs
@@ -67,9 +67,9 @@
             this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.listView1 = new System.Windows.Forms.ListView();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -97,6 +97,7 @@
             this.radioButton3.TabStop = true;
             this.radioButton3.Text = "Fahrzeug";
             this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
             // 
             // radioButton2
             // 
@@ -108,6 +109,7 @@
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "sonstiger Kalender";
             this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
             // 
             // radioButton1
             // 
@@ -120,6 +122,7 @@
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "Raum";
             this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
             // 
             // groupBox2
             // 
@@ -470,6 +473,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(324, 20);
             this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // groupBox3
             // 
@@ -522,15 +526,6 @@
             this.label3.TabIndex = 7;
             this.label3.Text = "Resultierende(s) Cmdlet(s)";
             // 
-            // listView1
-            // 
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(12, 308);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(776, 97);
-            this.listView1.TabIndex = 8;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(12, 411);
@@ -550,14 +545,23 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(152, 289);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(636, 106);
+            this.textBox2.TabIndex = 11;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 481);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.listView1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.textBox1);
@@ -621,9 +625,9 @@
         private System.Windows.Forms.CheckBox checkBox2;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- SharedMailboxes/Form1.cs
+++ SharedMailboxes/Form1.cs
@@ -6,11 +6,20 @@ namespace SharedMailboxes
 {
     public partial class Form1 : Form
     {
-        private String Script = "";
+        private String Script1a = "New-Mailbox -Name '";
+        private String Script1b = "Raum ";
+        private String Script1c = "WfbM-MW_";
+        private String Script1d = " Alias '";
+        private String Script1e = " -OrganizationalUnit '";
+        private String Script1f = "caritas-brilon-wfb.de/03_User/74_Raumpostfach_Raumkalender'";
+        private String Script1g = " -UserPrincipalName '";
+        private String Script1h = "MBDB_EX2014_L-Z' ";
+        private String Script1i = "-Room";
+
         public Form1()
         {
             InitializeComponent();
-            Script = "";
+            textBox2.Text = generateString();
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -20,11 +29,43 @@ namespace SharedMailboxes
 
         private String generateString()
         {
-            /*switch (true)
+            return Script1a + Script1b + Script1c + textBox1.Text  + "'" + Script1d + Script1c + textBox1.Text +
+                "'" + Script1e + Script1f + Script1g + Script1c + textBox1.Text + "@caritas-brilon-wfb.de'" +
+                "-SamAccountName '" + Script1c + textBox1.Text + "' -FirstName '' -Initials '' -LastName '' " +
+                "-Database '" + Script1h + Script1i;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //Script1e = Script1b + label2.Text + "'";
+            textBox2.Text = generateString();
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton3.Checked)
             {
-                default:
-            }*/
-            return Script;
+                Script1b = "Fzg-";
+                textBox2.Text = generateString();
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                Script1b = "Raum ";
+                textBox2.Text = generateString();
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                Script1b = "Kalender ";
+                textBox2.Text = generateString();
+            }
         }
     }
 }
