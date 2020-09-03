41553a51a13531d2bcc0cb11f73fb49a13bf497a
Markus Gärtner <gaertner.brilon@gmail.com>
2020-09-03 14:32:03 +0200

    Projektdateien hinzufügen.

---
+++ SharedMailboxes/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SharedMailboxes
+{
+    public partial class Form1 : Form
+    {
+        private String Script = "";
+        public Form1()
+        {
+            InitializeComponent();
+            Script = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private String generateString()
+        {
+            /*switch (true)
+            {
+                default:
+            }*/
+            return Script;
+        }
+    }
+}
